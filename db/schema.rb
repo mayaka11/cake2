6d4f6c0a228a8a9c9b7de622bc61c050bb31fa0d
EC2 Default User <mayoneko0@gmail.com>
2023-07-29 11:58:22 +0000

    admin/items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_25_113252) do
+ActiveRecord::Schema.define(version: 2023_07_29_115435) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2023_07_25_113252) do
     t.string "telephone_number"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "introduction"
   end
 
 end
