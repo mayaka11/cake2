3c4f61a0355b7f3c4f7bd024210bb4a25b6bc1dc
EC2 Default User <mayoneko0@gmail.com>
2023-08-05 12:04:11 +0000

    item完了

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,17 +2,22 @@ class Public::ItemsController < ApplicationController
 
 
   def index
-
+    @items = Item.all   #すべてのデータを表示するため、複数形にする。
   end
 
 
   def show
-
+    @item = Item.find(params[:id])   #1つのデータを取得するので、単数形にする。
   end
 
 
 
 
+
+private
+  def item_params
+    params.require(:item).permit(:name, :genre_id, :introduction, :price)
+  end
 
 
 end
